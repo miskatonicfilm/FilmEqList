5f744ac9a7558125f0e768c4085481b6643898dd
miskatonicfilm <miskatonicfilmdept@gmail.com>
2021-03-31 11:05:39 -0600

    Initial commit

---
+++ src/main/java/com/miskatonicfilm/FilmEqList/FilmEqListApplication.java
@@ -0,0 +1,13 @@
+package com.miskatonicfilm.FilmEqList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmEqListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmEqListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miskatonicfilm/FilmEqList/FilmEqListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miskatonicfilm.FilmEqList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmEqListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
